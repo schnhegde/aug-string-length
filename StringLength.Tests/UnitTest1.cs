9d6e2f16fd9e402507ffd147a7023cfb77ac27b7
Sachin Hegde <schnhegde@gmail.com>
2023-01-04 12:30:24 +0530

    inital commit

---
+++ StringLength.Tests/UnitTest1.cs
@@ -0,0 +1,31 @@
+using StringLength;
+
+namespace StringLength.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestEmpty()
+        {
+            Assert.Equal(0, LengthHelper.findLength(""));
+        }
+
+        [Fact]
+        public void TestStringLength1()
+        {
+            Assert.Equal(3, LengthHelper.findLength("123"));
+        }
+
+        [Fact]
+        public void TestStringLength2()
+        {
+            Assert.Equal(12, LengthHelper.findLength("123456789010"));
+        }
+
+        [Fact]
+        public void TestNullString()
+        {
+            Assert.Equal(0, LengthHelper.findLength(null));
+        }
+    }
+}
---
+++ StringLength/LengthHelper.cs
@@ -0,0 +1,19 @@
+﻿namespace StringLength
+{
+    public class LengthHelper
+    {
+        public static int findLength(string inputString)
+        {
+            if (inputString != null)
+            {
+                return inputString.Length;
+            }
+            return 0;
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine(LengthHelper.findLength("hello"));
+        }
+    }
+}
